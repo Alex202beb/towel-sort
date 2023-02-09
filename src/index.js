4adb9dfde5ff50468934f57c5030e41420b7c127
Alex202beb <holiwaa@yandex.ru>
2023-02-09 06:30:58 +0200

    feat: add sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let resArr = [];
+if (matrix) {
+matrix.map((e, i) => {
+if (i % 2 === 0) {
+e.map(a => resArr.push(a));
+} else {
+e.reverse().map(a => resArr.push(a));
+}
+});
+}
+return resArr;
 }
